a3064ebe3d2e5d3d84a15cb3f88804fc92d76671
Zotov Stanislav <zotovstanislav@mail.ru>
2024-03-05 00:21:47 +0300

    добавил миксин, который выводит информацию о созданном объекте в консоль

--- src/BasicClass.py
+++ src/BasicClass.py
@@ -6,3 +6,10 @@ class BasicClass(ABC):
 	@abstractmethod
 	def __str__(self):
 		pass
+
+
+class MixinInfo:
+	def __repr__(self):
+		#return f"{self.__class__.__name__}({', '.join(map(str, self.__dict__.values()))}"
+		info = ', '.join([f"{key}={value!r}" for key, value in self.__dict__.items()])
+		return f"{self.__class__.__name__}({info})"
