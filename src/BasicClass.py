a26200d5dabb0b64ccb45fc7f25a6bafca7e0ebd
Zotov Stanislav <zotovstanislav@mail.ru>
2024-03-05 22:14:17 +0300

    добавил абстрактный метод для создания продукта

--- src/BasicClass.py
+++ src/BasicClass.py
@@ -7,12 +7,16 @@ class BasicClass(ABC):
 	def __str__(self):
 		pass
 
+	@abstractmethod
+	def make_product(self):
+		pass
+
 
 class MixinInfo:
 	def __init__(self, *args):
 		print(repr(self))
 
 	def __repr__(self):
-		#return f"{self.__class__.__name__}({', '.join(map(str, self.__dict__.values()))}"
+		# return f"{self.__class__.__name__}({', '.join(map(str, self.__dict__.values()))}"
 		info = ', '.join([f"{key}={value!r}" for key, value in self.__dict__.items()])
 		return f"{self.__class__.__name__}({info})"
