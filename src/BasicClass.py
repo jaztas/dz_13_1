a0bbf61c438e6986ecef2518876434917ce95b9f
Zotov Stanislav <zotovstanislav@mail.ru>
2024-03-05 21:57:49 +0300

    добавил __init__ в миксин

--- src/BasicClass.py
+++ src/BasicClass.py
@@ -9,6 +9,9 @@ class BasicClass(ABC):
 
 
 class MixinInfo:
+	def __init__(self, *args):
+		print(repr(self))
+
 	def __repr__(self):
 		#return f"{self.__class__.__name__}({', '.join(map(str, self.__dict__.values()))}"
 		info = ', '.join([f"{key}={value!r}" for key, value in self.__dict__.items()])
